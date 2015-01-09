f118971efcd5019acdf0b122c984bade8981a90f
Andreas <ciscoheat@gmail.com>
2015-01-09 10:57:21 +0700

    Updated haxe.sh to automatically install haxelib.

--- src/haxe.sh
+++ src/haxe.sh
@@ -3,3 +3,4 @@ echo "=== Installing Haxe..."
 wget -q http://www.openfl.org/builds/haxe/haxe-3.1.3-linux-installer.tar.gz -O - | tar -xz
 sh install-haxe.sh -y >/dev/null 2>&1
 rm -f install-haxe.sh
+echo | haxelib setup
