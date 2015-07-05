b44915d1884f4cb2eff57ee96143ffc6811afed5
Andreas <ciscoheat@gmail.com>
2015-07-05 22:09:28 +0200

    Haxe targets upgrade.

--- src/haxe_targets.sh
+++ src/haxe_targets.sh
@@ -6,9 +6,12 @@ apt-get install -y gcc-multilib g++-multilib
 haxelib install hxcpp >/dev/null 2>&1
 
 echo "=== Installing C#..."
-apt-get install -y mono-devel
+apt-get install -y mono-devel mono-mcs
 haxelib install hxcs >/dev/null 2>&1
 
 echo "=== Installing Java..."
 apt-get install -y default-jdk
 haxelib install hxjava >/dev/null 2>&1
+
+echo "=== Installing PHP..."
+apt-get install -y php5-cli
