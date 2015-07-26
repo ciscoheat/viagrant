be87bcd425c1df426d943b14a7ff843fb1d50280
Andreas <ciscoheat@gmail.com>
2015-07-26 16:41:28 +0200

    Change endpoint binding for ArangoDB so the admin interface can be used with Vagrant port forwarding

--- src/arangodb.sh
+++ src/arangodb.sh
@@ -7,6 +7,9 @@ rm -f Release.key
 echo 'deb https://www.arangodb.com/repositories/arangodb2/xUbuntu_12.04/ /' >> /etc/apt/sources.list.d/arangodb.list
 apt-get update
 apt-get install arangodb=2.6.2 -y
+# Change endpoint binding so the admin interface can be used with Vagrant port forwarding
+sudo sed -i "s|^endpoint = tcp://127.0.0.1:8529|endpoint = tcp://0.0.0.0:8529|" /etc/arangodb/arangod.conf
+/etc/init.d/arangodb restart
 
 echo "ArangoDB server daemon (default HTTP port 8529)"
 echo "sudo /etc/init.d/arangodb <start|stop|status>"
