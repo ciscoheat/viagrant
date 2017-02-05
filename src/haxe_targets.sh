af8ac0988cd51bf5f3d5d27c9ae242e2a8eada25
Andreas <ciscoheat@gmail.com>
2017-02-05 21:03:40 +0100

    Starting on hashlink installation. Doesn't work for 32 bit architecture yet.

--- src/haxe_targets.sh
+++ src/haxe_targets.sh
@@ -22,3 +22,32 @@ apt-get install -y libcurl3:i386 libglib2.0-0:i386 libx11-6:i386 libxext6:i386 l
 
 echo "=== Installing Phantomjs (js testing)..."
 npm install -g phantomjs-prebuilt
+
+echo "=== Installing Hashlink"
+sudo apt-get install -y make cmake
+cd hashlink-1.1
+### ARCH=64 sudo apt-get install -y make cmake libvorbis-dev libz-dev zlib1g-dev libturbojpeg libpng-dev libsdl2-dev
+### ARCH=32
+dpkg --add-architecture i386
+apt-get update -y
+apt-get install libvorbis-dev:i386 -y
+apt-get install gcc-multilib libz-dev:i386 zlib1g-dev:i386 libturbojpeg:i386 libpng-dev:i386 libsdl2-dev:i386 libegl1-mesa-dev:i386 libgles2-m esa-dev:i386 libmirclient-dev:i386 libmircommon-dev:i386 libxkbcommon-dev:i386 -y
+sudo ldconfig
+###
+curl https://codeload.github.com/HaxeFoundation/hashlink/tar.gz/1.1 | tar xz
+rm CMakeCache.txt && rm cmake_install.cmake && rm Makefile
+cmake -DBUILD=make -DARCH=32 . && make && (
+	cd bin
+	cp -f hl /usr/bin
+	mkdir -p /usr/lib/hl
+	cp -f *.hdll /usr/lib/hl
+	cp -f libhl.so.0.1.0 /usr/lib/hl
+	ln -f -s /usr/lib/hl/libhl.so.0.1.0 /usr/lib/hl/libhl.so.0
+	ln -f -s /usr/lib/hl/libhl.so.0 /usr/lib/hl/libhl.so
+	ln -f -s /usr/lib/hl/libhl.so.0 /usr/lib/libhl.so.0
+	cd ..
+)
+cd ..
+
+# In /vagrant/bin/hl
+# gcc -o tests -D INCLUDE_ALL -std=c11 tests.c -Lhl -I. -Isrc
